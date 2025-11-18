191588472be8a1083be2d1a26f304917ba86452e
조연정 <jojoor201@gmail.com>
2025-11-18 21:35:00 +0900

    반응형

--- src/components/content5/ContentMid4.jsx
+++ src/components/content5/ContentMid4.jsx
@@ -10,19 +10,19 @@ const ContentMid4 = () => {
           <div className="mid4_subTitle">
             야놀자리서치의 더 많은 소식을 SNS로 만나보세요.
           </div>
-          <Link to={"/"}>
-            <div className="mid4_sub">구독신청하기</div>
-          </Link>
+          <div className="mid4_sns_wrapper">
+            {snsIcon.map((item) => {
+              return (
+                <Link>
+                  <img src={item.name} />
+                </Link>
+              );
+            })}
+          </div>
         </div>
-        <div className="mid4_sns_wrapper">
-          {snsIcon.map((item) => {
-            return (
-              <Link>
-                <img src={item.name} />
-              </Link>
-            );
-          })}
-        </div>
+        <Link to={"/"}>
+          <div className="mid4_sub">구독신청하기</div>
+        </Link>
       </div>
     </div>
   );
